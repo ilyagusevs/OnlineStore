1da11f93df7ef6eca382ddfb05ed59053852cc5c
s8_gusevs_i <s8_gusevs_i@venta.lv>
2021-03-19 17:49:26 +0200

    added new blade for filtering

--- resources/views/ajax/order-by.blade.php
+++ resources/views/ajax/order-by.blade.php
@@ -1,24 +1,23 @@
-@foreach($products as $product)
-                            
-                            @php
-                                $image = '';
-                                if(count($product->images) > 0){
-                                    $image = $product->images[0]['img'];
-                                }else{
-                                    $image = 'no_image.png';
+@foreach($products as $product)  
+    @php
+        $image = '';
+        if(count($product->images) > 0){
+            $image = $product->images[0]['img'];
+        }else{
+            $image = 'no_image.png';
                                 }
-                            @endphp
-                           <div class="product">
-                                <div class="product_image"><img src="/css/productImages/{{$image}}" alt="{{$product->title}}">
-                                    <div class="product_content">
-                                        <div class="product_title"><a href="{{route('showProduct',[$product->category['alias'], $product->id])}}">{{$product->title}}</a></div>
-                                        @if($product->new_price != null)
-                                            <div style="text-decoration: line-through">&euro; {{$product->old_price}}</div>
-                                            <div class="product_price">&euro; {{$product->new_price}}</div>
-                                        @else
-                                            <div class="product_price">&euro; {{$product->price}}</div>
-                                        @endif      
-                                    </div>    
-                                </div>
-                           </div>
+    @endphp
+    <div class="product">
+        <div class="product_image"><img src="/css/productImages/{{$image}}" alt="{{$product->title}}">
+            <div class="product_content">
+                <div class="product_title"><a href="{{route('showProduct',[$product->category['alias'], $product->id])}}">{{$product->title}}</a></div>
+                    @if($product->new_price != null)
+                        <div style="text-decoration: line-through">&euro; {{$product->old_price}}</div>
+                        <div class="product_price">&euro; {{$product->new_price}}</div>
+                    @else
+                        <div class="product_price">&euro; {{$product->price}}</div>
+                    @endif      
+            </div>    
+        </div>
+    </div>
 @endforeach
