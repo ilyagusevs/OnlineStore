b997c962c27f4c4350ab96779aa52a37b0ef3d63
s8_gusevs_i <s8_gusevs_i@venta.lv>
2021-03-19 17:51:11 +0200

    added js script for filtering

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -11,26 +11,26 @@
     <div class="home-title"><h1>{{$categ->title}}</h1></div>
 
     <div class="products">
-    <div class="container">
-            <div class="row">
-                <div class="col">
+        <div class="container">
+                <div class="row">
+                    <div class="col">
 
-                    <!-- Product Sorting -->
-                    <div class="sorting_bar d-flex flex-md-row flex-column align-items-md-center justify-content-md-start">
-                    <div class="results">Showing <span>{{$products->count()}}</span> results</div>
-                        <div class="sorting_container ml-md-auto">
-                            <div class="sorting">
-                                <ul class="item_sorting">
-                                    <li>
-                                        <span class="sorting_text">Sort by</span>
-                                        <i class="fa fa-chevron-down" aria-hidden="true"></i>
-                                        <ul>
-                                            <li class="product_sorting_btn" data-order="default"><span>Default</span></li>
-                                            <li class="product_sorting_btn" data-order="price-low-high"><span>Price: Low-High</span></li>
-                                            <li class="product_sorting_btn" data-order="price-high-low"><span>Price: High-Low</span></li>
-                                        </ul>
-                                    </li>
-                                </ul>
+                        <!-- Product Sorting -->
+                        <div class="sorting_bar d-flex flex-md-row flex-column align-items-md-center justify-content-md-start">
+                        <div class="results">Showing <span>{{$products->count()}}</span> results</div>
+                            <div class="sorting_container ml-md-auto">
+                                <div class="sorting">
+                                    <ul class="item_sorting">
+                                        <li>
+                                            <span class="sorting_text">Sort by</span>
+                                            <i class="fa fa-chevron-down" aria-hidden="true"></i>
+                                            <ul>
+                                                <li class="product_sorting_btn" data-order="price-low-high"><span>Price: Low-High</span></li>
+                                                <li class="product_sorting_btn" data-order="price-high-low"><span>Price: High-Low</span></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -64,12 +64,11 @@
                            </div>
                         @endforeach
                     </div>
-
                 </div>
             </div>
         </div>
     </div>
-
+    
 
 @endsection
 
@@ -78,6 +77,7 @@
         $(document).ready(function () {
             $('.product_sorting_btn').click(function () {
                 let orderBy = $(this).data('order')
+                $('.sorting_text').text($(this).find('span').text()) // чтобы отображалась сортировка в окошке, когда выбрали
                
                 $.ajax({
                     url: "{{route('showCategory',$categ->alias)}}",
@@ -90,7 +90,24 @@
                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     },
                     success: (data) => {
-                       console.log(data)
+                        let positionParameters = location.pathname.indexOf('?');
+                        let url = location.pathname.substring(positionParameters,location.pathname.length);
+                        let newURL = url + '?'; // http://127.0.0.1:8001/category/mens_clothing?
+                        newURL += 'orderBy=' + orderBy; // http://127.0.0.1:8001/category/mens_clothing?orderBy=price-low-high
+                        history.pushState({}, '', newURL);
+
+                        $('.product_grid').html(data)
+                        $('.product_grid').isotope('destroy')
+                        $('.product_grid').imagesLoaded( function() {
+                            let grid = $('.product_grid').isotope({
+                                itemSelector: '.product',
+                                layoutMode: 'fitRows',
+                                fitRows:
+                                    {
+                                        gutter: 30
+                                    }
+                            });
+                        });
                          
                     }
                 });
