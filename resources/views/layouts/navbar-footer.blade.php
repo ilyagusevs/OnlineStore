fcc5815f687e482fcc4fa203f644b84c8b083814
s8_gusevs_i <s8_gusevs_i@venta.lv>
2021-03-19 17:51:42 +0200

    designed and added new subcategories

--- resources/views/layouts/navbar-footer.blade.php
+++ resources/views/layouts/navbar-footer.blade.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>@yield('title')</title>
-        <link rel="stylesheet" href="/css/navbar-footer.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-        
-    </head>   
-    <body>
-      <div class="page">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <head>
+    <meta charset="utf-8" />
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/navbar-footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+    <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>    
+  </head>   
+  <body>
+    <div class="page">
+        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" style="font-size: 30px;" href="/">JUST SPORT</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -30,10 +31,8 @@
                         {{$category->title}}
                       </a>
                       <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        @if ($category->children)
-                        
-                            @foreach ($category->children as $child)
-                          
+                        @if ($category->childrenRecursive)
+                            @foreach ($category->childrenRecursive as $child)
                               <a class="dropdown-item" href="{{route('showCategory', $child->alias)}}">{{$child->title}}</a>
                             @endforeach
                         @endif
@@ -61,29 +60,86 @@
                 <a class="logout" href="/"></a>
             </div>
             
-        </nav>
-
-
-        
-
-        
-        @yield('content')
-        
-        <footer class="bg-light text-center text-white">
-          <div class="container p-4 pb-0">
-            <section class="mb-4">
-              <a href="https://www.facebook.com" class="facebook"></a>
-              <a href="https://www.instagram.com" class="instagram"></a>
-              <a href="https://www.twitter.com" class="twitter"></a>
-            </section>
-          </div>
-          <div class="text-center p-3" style="background-color: rgba(0, 0 , 0, 0.2);">
-            © 2021 Copyright:
-            <a class="text-white" href="/">justsport.lv</a>
-          </div>
-        </footer>
+        </nav> -->
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a style="font-size: 32px;" class="navbar-brand" href="/">JUST SPORT</a>
+      <div class="collapse navbar-collapse" id="main_nav">
+      <ul class="navbar-nav">
+          @foreach ($categories as $category)
+        <li class="nav-item dropdown has-megamenu">
+          <a a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {{$category->title}}
+          </a>
+            <div style="margin-top: -1px; width: 69%; margin-left: 300px;" class="dropdown-menu megamenu" role="menu">
+              <div class="row">
+                <div class="col-md-3">
+                  <div class="col-megamenu"> 
+                    @if ($category->children) 
+                      @foreach ($category->children as $child)
+                        <div class="categ">
+                          <h5 class="category-title">{{$child->title}}</h5>
+                          @foreach ($child->children as $child2)
+                            <a class="dropdown-item" href="{{route('showCategory', $child2->alias)}}">{{$child2->title}}</a>
+                          @endforeach  
+                        </div>
+                      @endforeach
+                    @endif
+                  </div>  <!-- col-megamenu.// -->
+                </div><!-- end col-3 -->   
+              </div><!-- end row --> 
+            </div> <!-- dropdown-mega-menu.// -->
+          @endforeach
+        </li>
+      </ul>
+    </div>
+    <div class="container h-100">
+      <div class="d-flex justify-content-center h-100">
+        <div class="searchbar">
+          <input class="search_input" type="text" name="" placeholder="Search for items and brands">
+          <a href="#" class="search_icon"><i class="fa fa-search"></i></a>
         </div>
-
+      </div>
+    </div>            
+        
+      <div class="profile">
+        <a class="cart" href="/cart"></a>
+        <a class="user-profile" href="/account"></a>
+        <a class="logout" href="/"></a>
+      </div>
+    </nav>
+        
+  @yield('content')
+          
+  <footer class="bg-light text-center text-white">
+    <div class="container p-4 pb-0">
+      <section class="mb-4">
+        <a href="https://www.facebook.com" class="facebook"></a>
+        <a href="https://www.instagram.com/latvian.cars/" class="instagram"></a>
+        <a href="https://www.twitter.com" class="twitter"></a>
+      </section>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0 , 0, 0.2);">
+      © 2021 Copyright:
+      <a class="text-white" href="/">justsport.lv</a>
+    </div>
+    </footer>
+  </div>
 @yield('custom_js')
     </body>     
 </html>
+
+<script>
+ $(document).ready(function(){
+  $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("slow");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );  
+});
+</script>
