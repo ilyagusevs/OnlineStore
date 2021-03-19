f79f940b959316ff35a9be089e9e5259e4fa2255
s8_gusevs_i <s8_gusevs_i@venta.lv>
2021-03-19 17:47:28 +0200

    added new columns in product seeder

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -14,13 +14,16 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i = 1; $i < 11; $i++)
-            DB::table('products')->insert([
+       
+        for($i = 1; $i < 21; $i++)
+            DB::table('products')->insert([ 
                 'title' => 'Product '.$i,
-                'price' => rand(50, 500),
-                'in_stock' => 1,
+                'brand' => 'NIKE',
+                'old_price' => rand(50, 500),
+                'new_price' => rand(40, 350),
+                'in_stock' => rand(0, 1),
                 'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ',
-            
+                'category_id' => rand(9, 16)
         ]);
     }
 }
