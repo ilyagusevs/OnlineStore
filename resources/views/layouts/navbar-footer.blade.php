19977d26c6107701e731b571566625cb27b8013e
s8_gusevs_i <s8_gusevs_i@venta.lv>
2021-03-20 16:24:52 +0200

    added icons from 'font awesome'

--- resources/views/layouts/navbar-footer.blade.php
+++ resources/views/layouts/navbar-footer.blade.php
@@ -9,14 +9,15 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-    <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>    
+    <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
   </head>   
   <body>
     <div class="page">
@@ -39,6 +40,7 @@
                           <h5 style="font-weight: bold;" class="category-title">{{$child->title}}</h5>
                           @foreach ($child->children as $child2)
                             <a style="margin:0;padding:0;"class="dropdown-item" href="{{route('showCategory', $child2->alias)}}">{{$child2->title}}</a>
+                            <!-- href="{{route('showCategory', [$category->alias, $child->alias, $child2->alias])}}"  -->
                           @endforeach  
                         </div>
                       @endforeach
@@ -61,9 +63,9 @@
     </div>            
         
       <div class="profile">
-        <a class="cart" href="/cart"></a>
-        <a class="user-profile" href="/account"></a>
-        <a class="logout" href="/"></a>
+        <a  class="fa fa-shopping-cart fa-2x" href="/cart"></a>
+        <a class="fa fa-user fa-2x" href="/account"></a>
+        <a class="fa fa-sign-out fa-2x" href="/"></a>
       </div>
     </nav>
         
